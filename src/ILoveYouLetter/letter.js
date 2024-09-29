403adf822202cd1530f729f262e6f30d0d531e29
Catherine <cxr248@student.bham.ac.uk>
2024-09-29 21:29:46 +0100

    Finished letter/scroll but need to get audio working so it keeps on repeating

--- src/ILoveYouLetter/letter.js
+++ src/ILoveYouLetter/letter.js
@@ -1,6 +1,30 @@
-function playMusic(){
-    let audio = Audio = new Audio("../../Media/glueSongPart2.mp3");
-    audio.play();
+let audio1;
+let audio2;
+function playMusic1(){
+    audio1 = Audio = new Audio("../../Media/glueSongPart2.mp3");
+    audio1.addEventListener('ended', () => {
+        playWholeSong();
+    })
+    audio1.play();
 }
 
-playMusic();
+function playWholeSong(){
+    audio2 = Audio = new Audio("../../Media/glueSong.mp3");
+
+    audio2.addEventListener('ended', () => {
+        audio2.currentTime = 0; // reset to beginning
+        audio2.play();
+    })
+    audio2.play();
+}
+
+window.addEventListener('load', playMusic1);
+
+
+let rolled = document.getElementById('beforeUnroll');
+let unrolled = document.getElementById('afterUnroll');
+
+rolled.addEventListener('click', () => {
+    rolled.classList.add('hidden');
+    unrolled.classList.remove('hidden');
+});
